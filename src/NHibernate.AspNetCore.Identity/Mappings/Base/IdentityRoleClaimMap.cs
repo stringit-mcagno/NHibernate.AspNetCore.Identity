f6398c38f063ae281ee9cfbf58a5e496a0bfaa7d
Matteo Cagno <matteo.cagno@gmail.com>
2021-09-24 16:12:57 +0200

    Released first version of NHibernate.AspNetCore.Identity.Generics

--- src/NHibernate.AspNetCore.Identity/Mappings/Base/IdentityRoleClaimMap.cs
+++ src/NHibernate.AspNetCore.Identity/Mappings/Base/IdentityRoleClaimMap.cs
@@ -6,22 +6,21 @@ namespace NHibernate.AspNetCore.Identity.Mappings.Base {
 
     public class IdentityRoleClaimMap<TKey> : ClassMap<IdentityRoleClaim<TKey>>
         where TKey : IEquatable<TKey> {
-        public IdentityRoleClaimMap() : this("RoleClaims", "Id") {
+        public IdentityRoleClaimMap() : this("RoleClaims", "Id")
+        {
         }
-
-
+        
         public IdentityRoleClaimMap(string tableName = "RoleClaims", string idColumnName = "Id")
-            {
-
-                Table(tableName);
-                Id(e => e.Id)
-                    .Column(idColumnName)
-                    .GeneratedBy.Native();
-                Map(e => e.ClaimType);
-                Map(e => e.ClaimValue);
-                Map(e => e.RoleId);
-            }
-
+        {
+            Table(tableName);
+            Id(e => e.Id)
+                .Column(idColumnName)
+                .GeneratedBy.Native();
+            Map(e => e.ClaimType);
+            Map(e => e.ClaimValue);
+            Map(e => e.RoleId);
         }
+
+    }
 
 }
